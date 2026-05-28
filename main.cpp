c60bc58d6b1e6d58d3262bfff180f52d8d55a8b8
dcortezc1-colabora <dcortezc1@miumg.edu.gt>
2026-05-28 02:04:37 -0600

    Primer avance del proyecto

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "=====================================\n";
+    cout << " SISTEMA DE PRESTAMO DE LIBROS\n";
+    cout << "=====================================\n";
+
+    cout << "\nPrograma funcionando correctamente.\n";
+
+    return 0;
+}
